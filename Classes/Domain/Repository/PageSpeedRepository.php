f107d791d3ed19e29db2029f294cddc0630ba7b8
Georg Ringer <georg.ringer@gmail.com>
2019-09-09 21:23:56 +0200

    [TASK] Remove wrong comment

--- Classes/Domain/Repository/PageSpeedRepository.php
+++ Classes/Domain/Repository/PageSpeedRepository.php
@@ -2,19 +2,6 @@
 
 namespace GeorgRinger\PageSpeed\Domain\Repository;
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
 use GeorgRinger\PageSpeed\Domain\Model\Dto\Configuration;
 use GeorgRinger\PageSpeed\Domain\Model\Response;
 use TYPO3\CMS\Core\Cache\CacheManager;
